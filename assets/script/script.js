ddcfecee4a6e261844f0ee9aa17e4917f04d9a0d
mayulsaka <mayumilhotesakaguchi@yahoo.co.jp>
2024-12-11 23:03:58 +0900

    l6lp2_241211v2(提出)

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,8 +2,9 @@ $('.visual').slick({
   dots: true,
   slidesToShow: 3,
   slidesToScroll:3,
-  autoplay: true,
-  autoplaySpeed: 2000,
+  // autoplay: true,
+  autoplay: false,
+  autoplaySpeed: 5500,
   responsive: [
     {
       breakpoint: 600, // 399px以下のサイズに適用
