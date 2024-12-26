4e13d010fbcb384844dc75446e20fd8f1ba81b7e
mayulsaka <mayumilhotesakaguchi@yahoo.co.jp>
2024-12-27 03:45:52 +0900

    L6lp2_241227(いったん修正完了)

--- assets/script/main.js
+++ assets/script/main.js
@@ -19,20 +19,11 @@
           nextArrow: '<img src="/assets/image/voice_arrow_right_circle_sp.svg" class="slide-arrow next-arrow sp" alt="">',
         },
       }, 
-      // {
-      //   breakpoint: 1200, 
-      //   settings: {
-      //     slidesToShow: 2,
-      //     slidesToScroll:2,
-      //     prevArrow: '<img src="/assets/image/voice_arrow_left_circle.svg" class="slide-arrow prev-arrow sp" alt="">',
-      //     nextArrow: '<img src="/assets/image/voice_arrow_right_circle.svg" class="slide-arrow next-arrow sp" alt="">',
-      //   },
-      // }, 
     ],
    });
 
 
-// ---------- TOPへ戻るボタン ---------- 
+// TOPへ戻るボタン
 
 $(document).ready(function () {
     const backToTopButton = $('#back-to-top');
@@ -45,6 +36,6 @@ $(document).ready(function () {
         }
     });
     backToTopButton.on('click', function () {
-        $('html, body').animate({ scrollTop: 0 }, 'slow');
+        $('html, body').animate({ scrollTop: 0 });
     });
 });
